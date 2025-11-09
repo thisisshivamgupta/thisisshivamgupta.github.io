47d09938c6a4402a28bc688e8c7bb0c960e8611c
Your Name <thisisshivamgupta@gmail.com>
2025-11-09 13:46:38 +0530

    Setup GitHub Actions for deployment

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -7,6 +7,3 @@ type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
-
-
-thisisshivamgupta.github.io
